6e95580c6c2b42a4c4461018ab8cc61cb3a32579
zsmelfry <zsmelfry@gmail.com>
2022-01-26 12:36:53 +0100

    step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-shop';
+export class AppComponent implements OnInit {
+  public title = 'angular-shop';
+  public product: any;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public ngOnInit() {
+    this.getProducts();
+  }
+
+  public getProducts() {
+    this.httpClient.get('https://fakestoreapi.com/products').subscribe((products: any) => {
+      console.log(products);
+      this.product = products[0];
+    });
+  }
 }
